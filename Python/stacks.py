153dab790b8f9f7713e3720e9771be90edbf0468
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-08-05 14:59:45 +0300

    Add initial implementation of stack in Python

---
+++ Python/stacks.py
@@ -0,0 +1,15 @@
+stack = []
+
+# Push items into stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+stack.append(5)
+
+print(stack)
+
+# Pop an item 
+x = stack.pop()
+print(x)
+print(stack)
